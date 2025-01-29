446ce5f390dad23d3f142989be14862b464356ea
Dudel <dudel@demogr.mpg.de>
2025-01-29 09:02:54 +0100

    Fixed lifetime risk, updated results

--- Code/2-analysis.R
+++ Code/2-analysis.R
@@ -106,6 +106,8 @@
     # Split data
     first <- data |> filter(wave%in%1:8)
     secon <- data |> filter(wave%in%9:15)
+    
+    ### For expectancies
   
     # Model
     fit1 <- dtms_fullfit(data=first,controls=controls)
@@ -144,15 +146,7 @@
     
     # 1: Difference to high educated, AVERAGE
     resexp1$diff <- resexp1$impaired-resexp1$impaired[resexp1$start=="AVERAGE" & resexp1$education==2]
-    
-    # 1: Risk
-    tmp1 <- dtms_risk(risk="impaired",dtms=dtms,matrix=Tm1_low,start_distr=S1_low)
-    tmp2 <- dtms_risk(risk="impaired",dtms=dtms,matrix=Tm1_med,start_distr=S1_med)
-    tmp3 <- dtms_risk(risk="impaired",dtms=dtms,matrix=Tm1_hig,start_distr=S1_hig)
-    resexp1$risk <- c(tmp1[1:3],tmp2[1:3],tmp3[1:3])
-    resexp1$riskdiff <- resexp1$risk-resexp1$risk[resexp1$start=="AVERAGE" & resexp1$education==2]
-    resexp1$time <- 1
-    
+
     # 2: Expectancies
     resexp2 <- rbind(
       data.frame(education=0,start=c("Not imp.","impaired","AVERAGE"),dtms_expectancy(dtms=dtms,matrix=Tm2_low,start_distr=S2_low)),
@@ -163,6 +157,48 @@
     # 2: Difference to high educated, AVERAGE
     resexp2$diff <- resexp2$impaired-resexp2$impaired[resexp2$start=="AVERAGE" & resexp2$education==2]
     
+    ### Risk
+    
+    # Re-estimate everything to get lifetime risk
+    first <- dtms_forward(data=first,state="impaired",dtms=dtms)
+    secon <- dtms_forward(data=secon,state="impaired",dtms=dtms)
+
+    # Model
+    fit1 <- dtms_fullfit(data=first,controls=controls)
+    fit2 <- dtms_fullfit(data=secon,controls=controls)
+
+    # Predict probabilities
+    probs1_low <- dtms_transitions(dtms=dtms,model=fit1,controls=low,se=F)
+    probs1_med <- dtms_transitions(dtms=dtms,model=fit1,controls=med,se=F)
+    probs1_hig <- dtms_transitions(dtms=dtms,model=fit1,controls=hig,se=F)
+    probs2_low <- dtms_transitions(dtms=dtms,model=fit2,controls=low,se=F)
+    probs2_med <- dtms_transitions(dtms=dtms,model=fit2,controls=med,se=F)
+    probs2_hig <- dtms_transitions(dtms=dtms,model=fit2,controls=hig,se=F)
+
+    # Transition matrices
+    Tm1_low <- dtms_matrix(dtms=dtms,probs=probs1_low)
+    Tm1_med <- dtms_matrix(dtms=dtms,probs=probs1_med)
+    Tm1_hig <- dtms_matrix(dtms=dtms,probs=probs1_hig)
+    Tm2_low <- dtms_matrix(dtms=dtms,probs=probs2_low)
+    Tm2_med <- dtms_matrix(dtms=dtms,probs=probs2_med)
+    Tm2_hig <- dtms_matrix(dtms=dtms,probs=probs2_hig)
+
+    # Starting distribution (correct, but small sample sizes)
+    S1_low <- dtms_start(dtms=dtms,data=first,start_time=c(50:56),variables=list(education=factor("0",levels=c("0","1","2"))))
+    S1_med <- dtms_start(dtms=dtms,data=first,start_time=c(50:56),variables=list(education=factor("1",levels=c("0","1","2"))))
+    S1_hig <- dtms_start(dtms=dtms,data=first,start_time=c(50:56),variables=list(education=factor("2",levels=c("0","1","2"))))
+    S2_low <- dtms_start(dtms=dtms,data=secon,start_time=c(50:56),variables=list(education=factor("0",levels=c("0","1","2"))))
+    S2_med <- dtms_start(dtms=dtms,data=secon,start_time=c(50:56),variables=list(education=factor("1",levels=c("0","1","2"))))
+    S2_hig <- dtms_start(dtms=dtms,data=secon,start_time=c(50:56),variables=list(education=factor("2",levels=c("0","1","2"))))
+    
+    # 1: Risk
+    tmp1 <- dtms_risk(risk="impaired",dtms=dtms,matrix=Tm1_low,start_distr=S1_low)
+    tmp2 <- dtms_risk(risk="impaired",dtms=dtms,matrix=Tm1_med,start_distr=S1_med)
+    tmp3 <- dtms_risk(risk="impaired",dtms=dtms,matrix=Tm1_hig,start_distr=S1_hig)
+    resexp1$risk <- c(tmp1[1:3],tmp2[1:3],tmp3[1:3])
+    resexp1$riskdiff <- resexp1$risk-resexp1$risk[resexp1$start=="AVERAGE" & resexp1$education==2]
+    resexp1$time <- 1
+    
     # 2: Risk
     tmp1 <- dtms_risk(risk="impaired",dtms=dtms,matrix=Tm2_low,start_distr=S2_low)
     tmp2 <- dtms_risk(risk="impaired",dtms=dtms,matrix=Tm2_med,start_distr=S2_med)
@@ -171,7 +207,8 @@
     resexp2$riskdiff <- resexp2$risk-resexp2$risk[resexp2$start=="AVERAGE" & resexp2$education==2]
     resexp2$time <- 2
     
-    # Differences over time
+    ### Differences over time
+    
     resexp1$diffdiff <- 0
     resexp1$riskdiffdiff <- 0
     
@@ -206,4 +243,8 @@
   
   save(list=c("men_res","women_res","men_boot","women_boot"),
        file="Results/over_time.Rda")
+  
+  # If running on workstation
+  if(Sys.info()["nodename"]%in%c("HYDRA01","HYDRA02","HYDRA11")) {rm(list=ls());gc()}
+  
   
--- Code/3-results.R
+++ Code/3-results.R
@@ -91,3 +91,88 @@
   # Combine
   total <- rbind(men_total,women_total)
   
+  
+### For supplementary materials ################################################
+  
+  # Parts of the table
+  men_exp <- men_res[men_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","not.impaired")]
+  men_ris <- men_res[men_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","TOTAL")]
+  
+  women_exp <- women_res[women_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","not.impaired")]
+  women_ris <- women_res[women_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","TOTAL")]
+  
+  men_exp_low <- summary(men_boot)$`2.5%`[men_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","not.impaired")]
+  men_exp_hig <- summary(men_boot)$`97.5%`[men_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","not.impaired")]
+  
+  men_ris_low <- summary(men_boot)$`2.5%`[men_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","TOTAL")]
+  men_ris_hig <- summary(men_boot)$`97.5%`[men_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","TOTAL")]
+  
+  women_exp_low <- summary(women_boot)$`2.5%`[women_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","not.impaired")]
+  women_exp_hig <- summary(women_boot)$`97.5%`[women_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","not.impaired")]
+  
+  women_ris_low <- summary(women_boot)$`2.5%`[women_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","TOTAL")]
+  women_ris_hig <- summary(women_boot)$`97.5%`[women_res[,"education"]%in%c(0,2),][c(3,6,9,12),c("education","time","TOTAL")]
+  
+  # Editing
+  men_exp <- reshape(as.data.frame(men_exp),direction="wide",idvar="education",timevar="time",v.names="not.impaired")
+  men_exp_low <- reshape(as.data.frame(men_exp_low),direction="wide",idvar="education",timevar="time",v.names="not.impaired")
+  men_exp_hig <- reshape(as.data.frame(men_exp_hig),direction="wide",idvar="education",timevar="time",v.names="not.impaired")
+  
+  men_ris <- reshape(as.data.frame(men_ris),direction="wide",idvar="education",timevar="time",v.names="TOTAL")
+  men_ris_low <- reshape(as.data.frame(men_ris_low),direction="wide",idvar="education",timevar="time",v.names="TOTAL")
+  men_ris_hig <- reshape(as.data.frame(men_ris_hig),direction="wide",idvar="education",timevar="time",v.names="TOTAL")
+  
+  women_exp <- reshape(as.data.frame(women_exp),direction="wide",idvar="education",timevar="time",v.names="not.impaired")
+  women_exp_low <- reshape(as.data.frame(women_exp_low),direction="wide",idvar="education",timevar="time",v.names="not.impaired")
+  women_exp_hig <- reshape(as.data.frame(women_exp_hig),direction="wide",idvar="education",timevar="time",v.names="not.impaired")
+  
+  women_ris <- reshape(as.data.frame(women_ris),direction="wide",idvar="education",timevar="time",v.names="TOTAL")
+  women_ris_low <- reshape(as.data.frame(women_ris_low),direction="wide",idvar="education",timevar="time",v.names="TOTAL")
+  women_ris_hig <- reshape(as.data.frame(women_ris_hig),direction="wide",idvar="education",timevar="time",v.names="TOTAL")
+  
+  # Combining (men)
+  names(men_exp_low) <- c("education","not.impaired.1_low","not.impaired.2_low")
+  names(men_exp_hig) <- c("education","not.impaired.1_hig","not.impaired.2_hig")
+  
+  names(men_ris_low) <- c("education","TOTAL.1_low","TOTAL.2_low")
+  names(men_ris_hig) <- c("education","TOTAL.1_hig","TOTAL.2_hig")
+  
+  men_total <- merge(men_exp,men_exp_low)
+  men_total <- merge(men_total,men_exp_hig)
+  men_total <- merge(men_total,men_ris)
+  men_total <- merge(men_total,men_ris_low)
+  men_total <- merge(men_total,men_ris_hig)
+  
+  varorder <- c("education",
+                "not.impaired.1","not.impaired.1_low","not.impaired.1_hig",
+                "TOTAL.1","TOTAL.1_low","TOTAL.1_hig",
+                "not.impaired.2","not.impaired.2_low","not.impaired.2_hig",
+                "TOTAL.2","TOTAL.2_low","TOTAL.2_hig")
+  
+  men_total <- men_total[,varorder]
+  men_total <- round(men_total,digits=2)
+  rownames(men_total) <- c("Men low","Men high")
+  men_total <- men_total[,-1]
+  
+  # Women
+  names(women_exp_low) <- c("education","not.impaired.1_low","not.impaired.2_low")
+  names(women_exp_hig) <- c("education","not.impaired.1_hig","not.impaired.2_hig")
+  
+  names(women_ris_low) <- c("education","TOTAL.1_low","TOTAL.2_low")
+  names(women_ris_hig) <- c("education","TOTAL.1_hig","TOTAL.2_hig")
+  
+  women_total <- merge(women_exp,women_exp_low)
+  women_total <- merge(women_total,women_exp_hig)
+  women_total <- merge(women_total,women_ris)
+  women_total <- merge(women_total,women_ris_low)
+  women_total <- merge(women_total,women_ris_hig)
+  
+  women_total <- women_total[,varorder]
+  women_total <- round(women_total,digits=2)
+  
+  rownames(women_total) <- c("Women low","Women high")
+  women_total <- women_total[,-1]
+  
+  # Combine
+  total <- rbind(men_total,women_total)
+  
